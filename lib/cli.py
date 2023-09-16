9d844e31d3a5105a48cd9039559a621e54f50977
Irvin Dinkins <irvdude44@gmail.com>
2023-09-16 13:48:19 -0400

    added cli functionality

--- lib/cli.py
+++ lib/cli.py
@@ -6,3 +6,7 @@ def main():
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
     print(f"You have selected {options[menu_entry_index]}!")
+
+
+if __name__ == "__main__":
+    main()
