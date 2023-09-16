68928cc8720941795b89b81ba207000620e5ee51
Irvin Dinkins <irvdude44@gmail.com>
2023-09-16 14:43:39 -0400

    building out acessesing db data in cli

--- lib/cli.py
+++ lib/cli.py
@@ -1,11 +1,15 @@
 from simple_term_menu import TerminalMenu
 
 
-def main():
-    options = ["entry1", "entry2", "entry3"]
+def main(self):
+    self.clear(44)
+    options = ["Person", "Chores"]
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
     print(f"You have selected {options[menu_entry_index]}!")
+
+
+# def select_person(self):
 
 
 if __name__ == "__main__":
